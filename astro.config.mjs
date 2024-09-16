cdbdb6d42ad5c298cbc128d17c72af3cdc026d6e
Manish Ranjan <manish.ranjan@gameskraft.com>
2024-09-17 00:37:03 +0530

    add netlify for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   site: 'http://localhost:3000',
   integrations: [tailwind()],
+  output: 'server',
+  adapter: netlify(),
 });
