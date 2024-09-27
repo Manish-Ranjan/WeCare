5036db6ab87fc87e033c0b0fd1b1406b17628f7b
Manish Ranjan <manish.ranjan@gameskraft.com>
2024-09-27 11:31:11 +0530

    fix prettier config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-import theme from '@config/theme.json';
+import theme from './src/config/theme.json';
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
